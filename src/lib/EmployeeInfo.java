3fa2cd1342c48b4026f9d697558873925eae6645
iqbalFatur87 <iqbalfatur18@gmail.com>
2023-04-15 05:18:57 +0700

    Use Gender and Nationality enums instead of boolean  for gender and isForeigner variables

--- src/lib/EmployeeInfo.java
+++ src/lib/EmployeeInfo.java
@@ -1,24 +1,29 @@
 package lib;
 
 public class EmployeeInfo {
+    enum Gender {
+        MALE,FEMALE
+    }
+    enum Nationality {
+        LOCAL,FOREIGNER
+    }
+
     private String employeeId;
 	private String firstName;
 	private String lastName;
 	private String idNumber;
 	private String address;
-	private boolean isForeigner;
-	private boolean gender;
-    
-    // constructors, getters, setters
-    public EmployeeInfo(String employeeId, String firstName, String lastName, String idNumber, String address, boolean isForeigner, boolean gender) {
+    private Gender gender;
+    private Nationality nationality;
+
+    public EmployeeInfo(String employeeId, String firstName, String lastName, String idNumber, String address,
+        Gender gender, Nationality nationality) {
         this.employeeId = employeeId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNumber = idNumber;
         this.address = address;
-        this.isForeigner = isForeigner;
         this.gender = gender;
+        this.nationality = nationality;
     }
-
-    
 }
